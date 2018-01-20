7d0b1487097a4340ee9d6c0eb22d2fc7266aa2c0
aniono <aniono@hotmail.com>
2018-01-20 18:48:01 +0800

    Add project files.

---
+++ DotNetCore.Learning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetCore.Learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
