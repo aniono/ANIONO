1eb80d285f44bf084cd4f6001765483f58b69191
Will Li <aniono@hotmail.com>
2019-06-22 15:53:58 +0800

    Merge sort.

---
+++ AlgorithmsAndDataStructures.Tests/MergeSortTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests
+{
+    public class MergeSortTests
+    {
+        private readonly MergeSort _objUnderTest;
+
+        public MergeSortTests()
+        {
+            _objUnderTest = new MergeSort();
+        }
+
+        [Fact]
+        public void Test_Sort()
+        {
+            // Arrange
+            const int NumberOfTestCases = 3;
+            var testCases = new int[NumberOfTestCases][]
+            {
+                new int[] { 3, 2, 4, 5, 3, 6, 9 },
+                new int[] { 1, 2, 3, 4, 5 },
+                new int[] { 5, 4, 3, 2, 1 },
+            };
+            var expected = new int[NumberOfTestCases][];
+            for(int i = 0; i < NumberOfTestCases; i++)
+            {
+                expected[i] = testCases[i].OrderBy(x => x).ToArray();                
+            }
+            
+            // Act
+            for(int i = 0; i < NumberOfTestCases; i++)
+            {
+                _objUnderTest.Sort(testCases[i], 0, testCases[i].Length - 1);
+            }
+
+            // Assert
+            for(int i=0;i<NumberOfTestCases;i++)
+            {
+                for(int j = 0; j < testCases[i].Length; j++)
+                {
+                    Assert.Equal(expected[i][j], testCases[i][j]);
+                }
+            }
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/MergeSort.cs
@@ -0,0 +1,41 @@
+﻿namespace AlgorithmsAndDataStructures
+{
+    public class MergeSort
+    {
+        public void Sort(int[] nums, int start, int end)
+        {
+            if (start < end)
+            {
+                int middle = (start + end) / 2;
+                Sort(nums, start, middle);
+                Sort(nums, middle + 1, end);
+                Merge(nums, start, middle, end);
+            }
+        }
+
+        private void Merge(int[] nums, int start, int middle, int end)
+        {
+            int[] left = new int[(middle - start + 1) + 1];
+            int[] right = new int[(end - middle) + 1];
+            int indexOfLeftGuard = left.Length - 1;
+            int indexOfRightGuard = right.Length - 1;
+            left[indexOfLeftGuard] = int.MaxValue;
+            right[indexOfRightGuard] = int.MaxValue;
+
+            for (int i = 0; i < indexOfLeftGuard; i++)
+            {
+                left[i] = nums[start + i];
+            }
+
+            for (int j = 0; j < indexOfRightGuard; j++)
+            {
+                right[j] = nums[middle + j + 1];
+            }
+
+            for (int i = 0, j = 0, k = start; k <= end; k++)
+            {
+                nums[k] = left[i] <= right[j] ? left[i++] : right[j++];
+            }
+        }
+    }
+}
