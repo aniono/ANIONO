35396497298006fecac50cc30c96b40f0ae87cea
Will Li <aniono@hotmail.com>
2019-09-05 18:45:15 +0800

    1.removed unused using. 2.implemented dispose pattern.

--- DesignPatterns/Creational/Singleton.cs
+++ DesignPatterns/Creational/Singleton.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DesignPatterns.Creational
+﻿namespace DesignPatterns.Creational
 {
     public class Singleton
     {
---
+++ DesignPatterns/DotNet/BasicDispose.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DesignPatterns.DotNet
+{
+    public class BasicDispose : IDisposable
+    {
+        private bool disposed;
+        private readonly BasicDispose resource;
+
+        public BasicDispose() => this.resource = new BasicDispose();
+
+        /// <summary>
+        /// An alias for Dispose()
+        /// </summary>
+        public void Close()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                if (resource != null)
+                    resource.Dispose();
+            }
+
+            disposed = true;
+        }
+    }
+}
