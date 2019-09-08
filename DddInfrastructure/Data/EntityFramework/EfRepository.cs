c75087763c6d2dc7247e20149919384f5420d579
Will Li <aniono@hotmail.com>
2019-09-08 19:05:41 +0800

    Add DDD common project DddInfrastructure.

---
+++ DddInfrastructure/Data/EntityFramework/EfRepository.cs
@@ -0,0 +1,86 @@
+﻿using ANIONO.DddCommon.DddApplicationCore.Interfaces;
+using ANIONO.DddCommon.DddApplicationCore.SeedWork;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ANIONO.DddCommon.DddInfrastructure.Data
+{
+    public class EfRepository<TEntity, TId> : IAsyncRepository<TEntity, TId>
+        where TEntity : Entity<TId>, IAggregateRoot
+        where TId : IEquatable<TId>
+    {
+        private readonly DbContext _dbContext;
+
+        public EfRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public EfRepository(DbContext dbContext, IUnitOfWork unitOfWork)
+            : this(dbContext)
+        {
+            UnitOfWork = unitOfWork;
+        }
+
+        public IUnitOfWork UnitOfWork { get; }
+
+        public virtual async Task<TEntity> GetAsync(TId id)
+        {
+            return await _dbContext.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<TEntity>> ListAllAsync()
+        {
+            return await _dbContext.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<TEntity>> ListAsync(ISpecification<TEntity> spec)
+        {
+            return await ApplySpecification(spec).ToListAsync();
+        }
+
+        public async Task<int> CountAsync(ISpecification<TEntity> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Add(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<int> UpdateAsync(TEntity entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<int> DeleteAsync(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Remove(entity);
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        private IQueryable<TEntity> ApplySpecification(ISpecification<TEntity> spec)
+        {
+            return SpecificationEvaluator<TEntity, TId>.GetQuery(_dbContext.Set<TEntity>().AsQueryable(), spec);
+        }
+
+        public async Task<int> DeleteAsync(TId id)
+        {
+            var entity = await _dbContext.Set<TEntity>().FindAsync(id);
+            if (entity != null)
+            {
+                return await DeleteAsync(entity);
+            }
+
+            return 0;
+        }
+    }
+}
