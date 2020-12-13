6b2a4bee97e286108479c5f244ac549d1cab70f9
Will Li <will@heals.asia>
2020-12-13 23:36:28 +0800

    MedianOfTwoSortedArrays

---
+++ LeetCodeProblems.Tests/MedianOfTwoSortedArraysTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeProblems.Tests
+{
+    public class MedianOfTwoSortedArraysTests
+    {
+        private readonly MedianOfTwoSortedArrays _objUnderTest;
+
+        public MedianOfTwoSortedArraysTests()
+        {
+            _objUnderTest = new MedianOfTwoSortedArrays();
+        }
+
+        [Fact]
+        public void Test_FindMedianSortedArrays()
+        {
+            int[] nums1 = new int[] { 1, 3 };
+            int[] nums2 = new int[] { 2 };
+
+            var expected = 2.0;
+            var actual = _objUnderTest.FindMedianSortedArrays(nums1, nums2);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test_FindMedianSortedArrays2()
+        {
+            int[] nums1 = new int[] { 1, 2 };
+            int[] nums2 = new int[] { 3, 4 };
+
+            var expected = 2.5;
+            var actual = _objUnderTest.FindMedianSortedArrays(nums1, nums2);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test_FindMedianSortedArrays3()
+        {
+            int[] nums1 = new int[] { 0, 0 };
+            int[] nums2 = new int[] { 0, 0 };
+
+            var expected = 0.0;
+            var actual = _objUnderTest.FindMedianSortedArrays(nums1, nums2);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test_FindMedianSortedArrays4()
+        {
+            int[] nums1 = new int[] { };
+            int[] nums2 = new int[] { 1 };
+
+            var expected = 1.0;
+            var actual = _objUnderTest.FindMedianSortedArrays(nums1, nums2);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test_FindMedianSortedArrays5()
+        {
+            int[] nums1 = new int[] { 2 };
+            int[] nums2 = new int[] { };
+
+            var expected = 2.0;
+            var actual = _objUnderTest.FindMedianSortedArrays(nums1, nums2);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ LeetCodeProblems/MedianOfTwoSortedArrays.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems
+{
+    /// <summary>
+    /// Given two sorted arrays nums1 and nums2 of size m and n respectively, 
+    /// return the median of the two sorted arrays.
+    /// Follow up: The overall run time complexity should be O(log (m+n)).
+    /// </summary>
+    public class MedianOfTwoSortedArrays
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            if (nums1.Length == 0 && nums2.Length == 0)
+                return 0.0;
+
+            if (nums1.Length == 0)
+                return FindMdedian(nums2);
+
+            if (nums2.Length == 0)
+                return FindMdedian(nums1);
+
+            int[] mergedArray = new int[nums1.Length + nums2.Length];
+
+            int i = 0;
+            int j = 0;
+            int k = 0;
+
+            while (i < nums1.Length && j < nums2.Length)
+            {
+                if (nums1[i] < nums2[j])
+                    mergedArray[k++] = nums1[i++];
+
+                else if (nums1[i] > nums2[j])
+                    mergedArray[k++] = nums2[j++];
+
+                else
+                {
+                    mergedArray[k++] = nums1[i++];
+                    mergedArray[k++] = nums2[j++];
+                }
+            }
+
+            while (i < nums1.Length)
+            {
+                mergedArray[k++] = nums1[i++];
+            }
+
+            while (j < nums2.Length)
+            {
+                mergedArray[k++] = nums2[j++];
+            }
+
+            return FindMdedian(mergedArray);
+        }
+
+        private double FindMdedian(int[] nums)
+        {
+            int mid = nums.Length / 2;
+
+            if (nums.Length % 2 != 0)
+                return nums[mid];
+
+            else
+                return (nums[mid - 1] + nums[mid]) / 2.0;
+        }
+    }
+}
